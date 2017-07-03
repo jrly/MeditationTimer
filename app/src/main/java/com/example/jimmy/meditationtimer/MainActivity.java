e38e14ecd46e8e647f6fe2606fda8d0a4e27be4a
jrly <jimmy4ly@gmail.com>
2017-07-02 23:35:49 -0700

    Initial commit

---
+++ app/src/main/java/com/example/jimmy/meditationtimer/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.jimmy.meditationtimer;
+
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    SeekBar timerSeekBar;
+    TextView timerTextView;
+    Button controlTimer;
+    Boolean counterActive = false;
+    CountDownTimer countDownTimer;
+    MediaPlayer mPlayer;
+
+    public void resetTimer(){
+        timerTextView.setText("0:30");
+        timerSeekBar.setProgress(30);
+        countDownTimer.cancel();
+        controlTimer.setText("Start Meditation");
+        timerSeekBar.setEnabled(true);
+        counterActive = false;
+    }
+
+    public void updateTimer(int secondsLeft){
+        int minutes = secondsLeft/60;
+        int seconds = secondsLeft - minutes * 60;
+
+        String secondString = Integer.toString(seconds);
+
+        if(seconds <= 9){
+            secondString = "0" + secondString;
+        }
+
+        timerTextView.setText( Integer.toString(minutes) + ":" + secondString);
+    }
+
+    public void controlTime(View view){
+
+        if (counterActive == false) {
+
+            counterActive = true;
+            timerSeekBar.setEnabled(false);
+            controlTimer.setText("Wake up");
+
+            countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {     //add 100 so the timer doesn't jump to next number
+
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    updateTimer((int) millisUntilFinished / 1000);
+                }
+
+                @Override
+                public void onFinish() {
+
+                    resetTimer();
+                    mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.mediationalert);
+                    mPlayer.start();
+                    }
+            }.start();
+        }
+        else{
+            resetTimer();
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        timerTextView = (TextView) findViewById(R.id.timer);
+        timerSeekBar = (SeekBar) findViewById(R.id.seekBar);
+        controlTimer = (Button)findViewById(R.id.controlTimer);
+
+        timerSeekBar.setMax(1800);
+        timerSeekBar.setProgress(30);
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    updateTimer(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+
+    }
+}
